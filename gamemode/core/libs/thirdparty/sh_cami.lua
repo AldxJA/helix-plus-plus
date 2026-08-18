32ca6a94d98015683aedf4e333a10c98134ab5ef
bloodycop <bloodycop6385@gmail.com>
2026-08-18 06:59:42 +0300

    Update sh_cami.lua

--- gamemode/core/libs/thirdparty/sh_cami.lua
+++ gamemode/core/libs/thirdparty/sh_cami.lua
@@ -299,17 +299,16 @@ end
 function CAMI.GetPlayersWithAccess(privilegeName, callback, targetPly,
 extraInfoTbl)
     local allowedPlys = {}
-    local allPlys = player.GetAll()
-    local countdown = #allPlys
+    local countdown = player.GetCount()
 
     local function onResult(ply, hasAccess, _)
         countdown = countdown - 1
 
-        if hasAccess then table.insert(allowedPlys, ply) end
+        if hasAccess then allowedPlys[#allowedPlys + 1] = ply end
         if countdown == 0 then callback(allowedPlys) end
     end
 
-    for _, ply in ipairs(allPlys) do
+    for _, ply in player.Iterator() do
         CAMI.PlayerHasAccess(ply, privilegeName,
             function(...) onResult(ply, ...) end,
             targetPly, extraInfoTbl)
